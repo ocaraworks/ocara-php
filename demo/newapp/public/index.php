65aa7d60e3a1454aef85c880501f8baad7253307
nakeer123 <linyhtianwa@163.com>
2019-08-26 21:57:25 +0800

    修改新建应用

--- demo/newapp/public/index.php
+++ demo/newapp/public/index.php
@@ -2,15 +2,16 @@
 /**
  * 程序执行开始时间
  */
+
 define('OC_EXECUTE_START_TIME', microtime(true));
 
 /*
  * 加载框架或自动加载
  */
 //require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
-require_once dirname(dirname(__DIR__)) . '/system/library/Ocara.php';
+require_once dirname(dirname(__DIR__)) . '/ocara/system/library/Core/Ocara.php';
 
 /*
  * 运行应用
  */
-Ocara\Ocara::create();
+Ocara\Core\Ocara::create();
