33e1b9b71c4b15ceaf75e541f9767db5c27ed4ba
nakeer123 <nakeer123@aliyun.com>
2019-10-31 01:57:42 +0800

    修改配置文件。。

--- system/library/Models/DatabaseModel.php
+++ system/library/Models/DatabaseModel.php
@@ -297,7 +297,7 @@ abstract class DatabaseModel extends ModelBase
 
         $ref = new ReflectionObject($this);
         $filePath = ocCommPath($ref->getFileName());
-        $file = substr(basename($filePath), 0, -9);
+        $file = substr(basename($filePath), 0, -9) . '.php';
         $dir = dirname($filePath) . OC_DIR_SEP;
 
         if ($this->module) {
