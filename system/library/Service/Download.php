9b5913b8ce93eb052d16671480c0cdc27fe54858
nakeer123 <nakeer123@aliyun.com>
2019-11-28 22:48:43 +0800

    修改下载。

--- system/library/Service/Download.php
+++ system/library/Service/Download.php
@@ -23,12 +23,11 @@ class Download extends ServiceBase
      */
 	public function download($filePath, $saveName = null, $encode = 'utf-8')
 	{
-		$expression = '/^.+\.(\w{2,4})$/';
-		$mineTypes = ocConfig('MINE_TYPES');
+        $expression = '/^.+\.(\w{2,4})$/';
 
-		if (!preg_match($expression, $filePath)) {
-			$this->showError('fault_path');
-		}
+        if (!preg_match($expression, $filePath)) {
+            $this->showError('fault_path');
+        }
 
 		if (!ocFileExists($filePath)) {
 			$this->showError('not_exists_file');
@@ -39,28 +38,46 @@ class Download extends ServiceBase
 			$saveName = end($options);	
 		}
 
-		if (!preg_match($expression, $saveName, $mt)) {
-			$this->showError('failed_save_filename');
-		}
-		
-		$saveName = $saveName ? : ocBasename($filePath);
-		$saveType = $mt[1];
-
-		if (!array_key_exists($saveType, $mineTypes)) {
-			$this->showError('invalid_download_type', array($saveType));
-		}
-		
-		$mine = $mineTypes[$saveType];
 		$content = ocRead($filePath);
-
-		if (!$content) return false;
-		
-		header("Content-Type:{$mine};encoding={$encode};name={$saveName}");
-		header("Content-Disposition: attachment; filename={$saveName}");
-		header('Pragma: no-cache');
-		header('Cache-Control: no-cache, must-revalidate');
-		header('Expires: 0');
-		
-		echo ($content);
+		$this->downloadContent($saveName, $content, $encode);
 	}
+
+    /**
+     * 下载内容
+     * @param $content
+     * @param $saveName
+     * @param string $encode
+     * @return bool
+     * @throws Exception
+     */
+	public function downloadContent($content, $saveName, $encode = 'utf-8')
+    {
+        if (!$content) return false;
+
+        $expression = '/^.+\.(\w{2,4})$/';
+
+        if (!preg_match($expression, $saveName, $mt)) {
+            $this->showError('failed_save_filename');
+        }
+
+        $saveName = $saveName ? : ocBasename($filePath);
+        $saveType = $mt[1];
+        $mineTypes = ocConfig('MINE_TYPES');
+
+        if (!array_key_exists($saveType, $mineTypes)) {
+            $this->showError('invalid_download_type', array($saveType));
+        }
+
+        $mine = $mineTypes[$saveType];
+        
+        ocService()->response->setHeader(array(
+            "Content-Type:{$mine};encoding={$encode};name={$saveName}",
+            "Content-Disposition: attachment; filename={$saveName}",
+            'Pragma: no-cache',
+            'Cache-Control: no-cache, must-revalidate',
+            'Expires: 0',
+        ));
+
+        ocService()->response->setBody($content);
+    }
 }
