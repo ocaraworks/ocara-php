32e7d0a6df4d8a4dd36b9d666da9ceead5beef4d
nakeer <nakeer123@aliyun.com>
2020-02-20 00:30:32 +0800

    修改关键字配置格式。

--- system/library/Core/DatabaseBase.php
+++ system/library/Core/DatabaseBase.php
@@ -96,7 +96,7 @@ class DatabaseBase extends Base
         if (!$config['keywords']) {
             $config['keywords'] = array();
         } else {
-            $keywords = is_array($config['keywords']) ? $config['keywords']: explode(',', $config['keywords']);
+            $keywords = is_array($config['keywords']) ? $config['keywords']: array();
             $config['keywords'] = array_map('trim', $keywords);
         }
 
