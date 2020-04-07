8b45de02e6aba56ea4cc1375911693e295ca98aa
nakeer <nakeer123@aliyun.com>
2020-04-07 21:48:37 +0800

    增加处理函数

--- system/library/Handlers/UrlHandler.php
+++ system/library/Handlers/UrlHandler.php
@@ -8,7 +8,45 @@
 namespace Ocara\Handlers;
 
 use Ocara\Core\Base;
+use Ocara\Core\Event;
 
 class UrlHandler extends Base
 {
+    /**
+     * 解析URL
+     * @param int $urlType
+     * @param string $paramsString
+     * @param Event $event
+     * @param object $eventTarget
+     * @return array
+     */
+    public function parseUrlParams($urlType, $paramsString, $event, $eventTarget)
+    {
+        return array();
+    }
+
+    /**
+     * 新建URL
+     * @param int $urlType
+     * @param string|array $route
+     * @param array $params
+     * @return null
+     */
+    public function createUrl($urlType, $route, $params)
+    {
+        return null;
+    }
+
+    /**
+     * 追加查询字符串参数
+     * @param int $urlType
+     * @param array $urlParams
+     * @param array $urlInfo
+     * @param array $params
+     * @return null
+     */
+    public function appendQueryParams($urlType, $urlParams, $urlInfo, $params)
+    {
+        return null;
+    }
 }
